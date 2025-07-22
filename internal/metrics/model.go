7892ab67c03e32a5e169c3e6354ed221d6017e70
Arthur Schurhaus <arthurschur@icloud.com>
2025-07-22 16:23:22 -0300

    started observabillity tool

---
+++ internal/metrics/model.go
@@ -0,0 +1,8 @@
+package metrics
+
+type MetricsReceived struct {
+	CPUUsage    float64
+	MemoryUsage float64
+	DiskUsage   float64
+	NetworkIO   float64
+}
---
+++ internal/websocket/listener.go
@@ -0,0 +1,57 @@
+package websocket
+
+import (
+	"go-observability-tool/internal/metrics"
+
+	"github.com/gorilla/websocket"
+)
+
+type Listener struct {
+	connections []*websocket.Conn
+	metricsChan chan metrics.MetricsReceived
+}
+
+func NewListener(metricsChan chan metrics.MetricsReceived) *Listener {
+	return &Listener{
+		connections: make([]*websocket.Conn, 0),
+		metricsChan: metricsChan,
+	}
+}
+
+func (l *Listener) AddConnection(conn *websocket.Conn) {
+	l.connections = append(l.connections, conn)
+}
+
+func (l *Listener) RemoveConnection(conn *websocket.Conn) {
+	for i, c := range l.connections {
+		if c == conn {
+			l.connections = append(l.connections[:i], l.connections[i+1:]...)
+			break
+		}
+	}
+}
+
+func (l *Listener) GetMetricsFromConnection(conn *websocket.Conn) metrics.MetricsReceived {
+	var metrics metrics.MetricsReceived
+	err := conn.ReadJSON(&metrics)
+	if err != nil {
+		return metrics
+	}
+
+	return metrics
+}
+
+func (l *Listener) Listen() {
+	for _, conn := range l.connections {
+		go func(c *websocket.Conn) {
+			for {
+				var metrics metrics.MetricsReceived
+				err := c.ReadJSON(&metrics)
+				if err != nil {
+					break
+				}
+				l.metricsChan <- metrics
+			}
+		}(conn)
+	}
+}
